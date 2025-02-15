efa6b99b113ed06fec6ee94ca67bfd66136d9e8f
AdarshVishwakarma <38201144+proaddy@users.noreply.github.com>
2025-02-15 23:21:11 +0530

    connecting CLI and GUI

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from cli.commands import main
+from cli.commands import main as cli_main
+from gui.main import main as gui_main
+import sys
+
+def main() -> None:
+    if len(sys.argv) > 1 and (sys.argv[1] == "gui" or sys.argv[1] == "GUI" or sys.argv[1] == 'interactive'):
+        gui_main()
+    else:
+        cli_main()
 
 if __name__ == "__main__":
     main()
